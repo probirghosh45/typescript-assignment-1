467bc305b886edf849b791e7a604418d638ac73c
Probir Ghosh <probirghosh@outlook.com>
2025-05-08 09:12:44 +0600

    problem 3 & 4 solved :hammer:

--- index.ts
+++ index.ts
@@ -10,3 +10,20 @@ function formatString(input: string, toUpper?: boolean): string {
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[]{
     return items.filter(item => item.rating >=4)
 }
+
+
+class Vehicle {
+    constructor(private make: string, private year: number) {}
+      getInfo():string {
+          return `Make: ${this.make}, Year: ${this.year}`
+      }
+  }
+  
+  class Car extends Vehicle {
+      constructor(make: string, year: number, private model: string) {
+          super(make, year);
+      }
+      getModel():string{
+          return `Model: ${this.model}`
+      }
+  }
